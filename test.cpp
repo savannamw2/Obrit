d26d5cfd29d299cc70580554be7437b2dc2eca9d
Savanna Whittaker <129790884+savannamw2@users.noreply.github.com>
2025-02-20 20:30:39 -0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,33 @@
+/***********************************************************************
+ * Source File:
+ *    Test : Test runner
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    The test runner for all the unit tests
+ ************************************************************************/
+
+#include "test.h"
+#include "testPosition.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <iostream>
+using namespace std;
+#endif
+
+/*****************************************************************
+ * TEST RUNNER
+ * Runs all the unit tests
+ ****************************************************************/
+void testRunner()
+{
+#ifdef _WIN32
+   AllocConsole();
+   FILE* stream;
+   errno_t err;
+   err = freopen_s(&stream, "CONOUT$", "a", stdout);
+#endif // _WIN32
+
+   TestPosition().run();
+}
---
+++ testPosition.h
@@ -0,0 +1,235 @@
+/***********************************************************************
+ * Header File:
+ *    Test Position : Test the Position class
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    All the unit tests for Position
+ ************************************************************************/
+
+
+#pragma once
+
+#include "position.h"
+#include "unitTest.h"
+
+/*******************************
+ * TEST Position
+ * A friend class for Position which contains the Position unit tests
+ ********************************/
+class TestPosition : public UnitTest
+{
+public:
+   void run()
+   {
+      // Constructors
+      construct_default();
+      construct_nonDefault();
+      construct_copy();
+      assign();
+ 
+      // Setters
+      setMetersX();
+      setMetersY();
+      
+      // Getters
+      getMetersX();
+      getMetersY();
+
+      report("Position");
+   }
+   
+private:
+
+   /*********************************************
+    * name:    DEFAULT CONSTRUCTOR
+    * input:   nothing
+    * output:  pos=(0, 0))
+    *********************************************/
+   void construct_default()
+   {
+      // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      // exercise
+      Position pos;
+      // verify
+      assertEquals(pos.x, 0.0);
+      assertEquals(pos.y, 0.0);
+      assertEquals(pos.metersFromPixels, 99.9);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+   /*********************************************
+    * name:    NON-DEFAULT CONSTRUCTOR
+    * input:   x=120.0, y=360.0
+    * output:  pos=(120.0, 360.0)
+    *********************************************/
+   void construct_nonDefault()
+   {
+      // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      double x = 120.0;
+      double y = 360.0;
+      // exercise
+      Position pos(x, y);
+      // verify
+      assertEquals(pos.x, 120.0);
+      assertEquals(pos.y, 360.0);
+      assertEquals(pos.metersFromPixels, 99.9);
+      assertEquals(x, 120.0);
+      assertEquals(y, 360.0);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+   /*********************************************
+    * name:    COPY CONSTRUCTOR
+    * input:   posRHS = (1234.5, 6789.0)
+    * output:  pos = (1234.5, 6789.0)
+    *********************************************/
+   void construct_copy()
+   {
+      // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      Position posRHS;
+      posRHS.x = 1234.5;
+      posRHS.y = 6789.0;
+      // exercise
+      Position pos(posRHS);
+      // verify
+      assertEquals(posRHS.x, 1234.5);
+      assertEquals(posRHS.y, 6789.0);
+      assertEquals(posRHS.metersFromPixels, 99.9);
+      assertEquals(pos.x, 1234.5);
+      assertEquals(pos.y, 6789.0);
+      assertEquals(pos.metersFromPixels, 99.9);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+   /*********************************************
+    * name:    ASSIGNMENT OPERATOR
+    * input:   posLHS=(-99.9, -88.8)  posRHS=(24.68, -13,57)
+    * output:  posLHS=(24.68, -13,57)
+    *********************************************/
+   void assign()
+   {
+      // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      Position posLHS;
+      posLHS.x = -99.9;
+      posLHS.y = -88.8;
+      Position posRHS;
+      posRHS.x = 24.68;
+      posRHS.y = -13.57;
+      // exercise
+      posLHS = posRHS;
+      // verify
+      assertEquals(posRHS.x, 24.68);
+      assertEquals(posRHS.y, -13.57);
+      assertEquals(posRHS.metersFromPixels, 99.9);
+      assertEquals(posLHS.x, 24.68);
+      assertEquals(posLHS.y, -13.57);
+      assertEquals(posLHS.metersFromPixels, 99.9);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+   
+   /*********************************************
+    * name:    GET METERS X
+    * input:   pos=(4500.0, 2500.0)
+    * output:  x=4500.0
+    *********************************************/
+   void getMetersX()
+   {
+      // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      Position pos;
+      pos.x = 4500.0;
+      pos.y = 2500.0;
+      // exercise
+      double x = pos.getMetersX();
+      // verify
+      assertEquals(x, 4500.0);
+      assertEquals(pos.x, 4500.0);
+      assertEquals(pos.y, 2500.0);
+      assertEquals(pos.metersFromPixels, 99.9);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+   /*********************************************
+    * name:    GET METERS Y
+    * input:   pos=(4500.0, 2500.0)
+    * output:  y=4500.0
+    *********************************************/
+   void getMetersY()
+   {
+      // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      Position pos;
+      pos.x = 4500.0;
+      pos.y = 2500.0;
+      // exercise
+      double y = pos.getMetersY();
+      // verify
+      assertEquals(pos.x, 4500.0);
+      assertEquals(pos.y, 2500.0);
+      assertEquals(pos.metersFromPixels, 99.9);
+      assertEquals(y, 2500.0);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+   /*********************************************
+    * name:    SET METERS Y
+    * input:   pos=(999.9, 888.8) x = 123.4
+    * output:  pos=(123.4, 888.8)
+    *********************************************/
+   void setMetersX()
+   {  // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      Position pos;
+      pos.x = 999.9;
+      pos.y = 888.8;
+      // exercise
+      pos.setMetersX(123.4);
+      // verify
+      assertEquals(pos.x, 123.4);
+      assertEquals(pos.y, 888.8);
+      assertEquals(pos.metersFromPixels, 99.9);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+   /*********************************************
+    * name:    GET METERS Y ZOOM
+    * input:   pos=(999.9, 888.8) y = 123.4
+    * output:  pos=(999.9, 123.4)
+    *********************************************/
+   void setMetersY()
+   {  // setup
+      double metersFromPixels = Position::metersFromPixels;
+      Position::metersFromPixels = 99.9;
+      Position pos;
+      pos.x = 999.9;
+      pos.y = 888.8;
+      // exercise
+      pos.setMetersY(123.4);
+      // verify
+      assertEquals(pos.x, 999.9);
+      assertEquals(pos.y, 123.4);
+      assertEquals(pos.metersFromPixels, 99.9);
+      // teardown
+      Position::metersFromPixels = metersFromPixels;
+   }
+
+};
